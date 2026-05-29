136f6179617659d0d3f30d256290e62f19d98787
Joaquín Pella <jpella@frba.utn.edu.ar>
2026-05-28 21:39:38 -0300

    script completo, realizar verificacion final y probarlo, ademas, realizar estrategia pdf

--- script_creacion_inicial.sql
+++ script_creacion_inicial.sql
@@ -540,3 +540,321 @@ begin
 end
 go
 
+create procedure [DB_LOPERS].Migrar_Agente
+as
+begin
+    insert into [DB_LOPERS].Agente (Legajo, Agencia_Nro_Agencia, ID_Localidad, Nombre, Apellido, Dni, Fecha_Nac, Telefono, Mail, Direccion)
+
+    select distinct 
+        m.Agente_Legajo,
+        m.Agencia_Nro_Agencia,
+        l.ID_Localidad,
+        m.Agente_Nombre,
+        m.Agente_Apellido,
+        m.Agente_Dni,
+        m.Agente_Fecha_Nac,
+        m.Agente_Telefono,
+        m.Agente_Mail,
+        m.Agente_Direccion
+    from gd_esquema.Maestra m 
+    join [DB_LOPERS].Localidad l on l.Nombre = m.Agente_Localidad
+    where m.Agente_Legajo is not null;
+end
+go
+
+create procedure [DB_LOPERS].Migrar_Hospedaje
+as
+begin
+    insert into [DB_LOPERS].Hospedaje (ID_Pais, ID_Ciudad, Nombre, Direccion, Incluye_Desayuno, Check_In, Check_Out)
+
+    select distinct 
+        p.ID_Pais,
+        c.ID_Ciudad,
+        m.Hospedaje_Nombre,
+        m.Hospedaje_Direccion,
+        m.Hospedaje_Incluye_Desayuno,
+        m.Hospedaje_Check_In,
+        m.Hospedaje_Check_Out
+    from gd_esquema.Maestra m
+    join [DB_LOPERS].Pais p on p.Nombre = m.Hospedaje_Pais
+    join [DB_LOPERS].Ciudad c on c.Nombre = m.Hospedaje_Ciudad
+    where m.Hospedaje_Nombre is not null;
+end
+go
+
+create procedure [DB_LOPERS].Migrar_Habitacion
+as
+begin
+    insert into [DB_LOPERS].Habitacion (ID_Hospedaje, Nombre, Descripcion, Precio_Noche)
+
+    select distinct
+        h.ID_Hospedaje,
+        m.Habitacion_Nombre,
+        m.Habitacion_Descripcion,
+        m.Habitacion_Precio_Noche
+    from gd_esquema.Maestra m
+    join [DB_LOPERS].Hospedaje h on h.Nombre = m.Habitacion_Nombre and h.Direccion = m.Hospedaje_Direccion
+    where m.Habitacion_Nombre is not null; 
+end
+go
+
+create procedure [DB_LOPERS].Migrar_Excursion
+as
+begin 
+    insert into [DB_LOPERS].Excursion (ID_Proveedor, Nombre, Descripcion, Horario, Duracion, Precio)
+
+    select distinct 
+        p.ID_Proveedor,
+        m.Excursion_Nombre,
+        m.Excursion_Descripcion,
+        m.Excursion_Horario,
+        m.Excursion_Duracion,
+        m.Excursion_Precio
+    from gd_esquema.Maestra m
+    join [DB_LOPERS].Proveedor p on p.Nombre = m.Proveedor_Nombre
+    where m.Excursion_Nombre is not null;
+end
+go
+
+create procedure [DB_LOPERS].Migrar_Vuelo
+as
+begin
+    insert into [DB_LOPERS].Vuelo (Aerolinea_Codigo, Aeropuerto_Salida_Codigo, Aeropuerto_Llegada_Codigo, Fecha_Salida, Horario_Salida, Fecha_Llegada, Horario_Llegada, Duracion, Precio, Incluye_Carry, Incluye_Valija)
+
+    select distinct 
+        m.Aerolinea_Codigo,
+        m.Aeropuerto_Salida_Codigo,
+        m.Aeropuerto_Llegada_Codigo,
+        m.Vuelo_Fecha_Salida,
+        m.Vuelo_Horario_Salida,
+        m.Vuelo_Fecha_Llegada,
+        m.Vuelo_Horario_Llegada,
+        m.Vuelo_Duracion,
+        m.Vuelo_Precio,
+        m.Vuelo_Incluye_Carry,
+        m.Vuelo_Incluye_Valija
+    from gd_esquema.Maestra m
+    where m.Vuelo_Fecha_Salida is not null;
+end
+go
+
+create procedure [DB_LOPERS].Migrar_Solicitud
+as
+begin 
+    insert into [DB_LOPERS].Solicitud (Nro_Solicitud, ID_Cliente, Agente_Legajo, Fecha_Solicitud, Fecha_Inicio_Tentativa, Fecha_Fin_Tentativa, Cant_Pasajeros, Observaciones, Presupuesto_Estimado)
+
+    select distinct 
+        m.Solicitud_Nro_Solicitud,
+        c.ID_Cliente,
+        m.Agente_Legajo,
+        m.Solicitud_Fecha_Solicitud,
+        m.Solicitud_Fecha_Inicio_Tentativa,
+        m.Solicitud_Fecha_Fin_Tentativa,
+        m.Solicitud_Cant_Pax,
+        m.Solicitud_Observaciones,
+        m.Solicitud_Presupuesto_Estimado
+    from gd_esquema.Maestra m
+    join [DB_LOPERS].Cliente c on c.Dni = m.Cliente_Dni
+    where m.Solicitud_Nro_Solicitud is not null;
+end
+go
+
+create procedure [DB_LOPERS].Migrar_Detalle_Solicitud
+as 
+begin
+    insert into [DB_LOPERS].Detalle_Solicitud (Solicitud_Nro_Solicitud, ID_Ciudad, Cant_Dias_Aprox, Observaciones)
+
+    select distinct 
+        m.Solicitud_Nro_Solicitud,
+        c.ID_Ciudad,
+        m.Detalle_Solicitud_Cant_Dias_Aprox,
+        m.Detalle_Solicitud_Observaciones
+    from gd_esquema.Maestra m
+    join [DB_LOPERS].Ciudad c on c.Nombre = m.Detalle_Solicitud_Ciudad
+    where m.Detalle_Solicitud_Ciudad is not null;
+end 
+go
+
+
+create procedure [DB_LOPERS].Migrar_Estado_Propuesta
+as
+begin
+    insert into [DB_LOPERS].Propuesta (Nro_Propuesta, Solicitud_Nro_Solicitud, Agente_Legajo, ID_Estado_Propuesta, Fecha_Emision, Vigencia_Hasta, Fecha_Desde, Fecha_Hasta, Subtotal, Descuento, Importe_Total)
+    select distinct
+        m.Propuesta_Nro_Propuesta,
+        m.Solicitud_Nro_Solicitud, 
+        m.Agente_Legajo, 
+        ep.ID_Estado_Propuesta, 
+        m.Propuesta_Fecha_Emision, 
+        m.Propuesta_Vigencia_Hasta, 
+        m.Propuesta_Fecha_Desde, 
+        m.Propuesta_Fecha_Hasta, 
+        m.Propuesta_Subtotal, 
+        m.Propuesta_Descuento, 
+        m.Propuesta_Importe_Total
+    from gd_esquema.Maestra m
+    join [DB_LOPERS].Estado_Propuesta ep on m.Propuesta_Estado = ep.Descripcion
+    where m.Propuesta_Nro_Propuesta is not null;
+end
+go
+
+create procedure [DB_LOPERS].Migrar_Propuesta_Vuelo
+as 
+begin   
+    insert into [DB_LOPERS].Propuesta_Vuelo (Propuesta_Nro_Propuesta, ID_Vuelo, Cant_Pasajes, Precio, Subtotal)
+    select distinct
+        m.Propuesta_Nro_Propuesta, 
+        v.ID_Vuelo, 
+        m.Detalle_Propuesta_Vuelo_Cant_Pasajes, 
+        m.Detalle_Propuesta_Vuelo_Precio, 
+        m.Detalle_Propuesta_Vuelo_Subtotal
+    from gd_esquema.Maestra m
+    join [DB_LOPERS].Vuelo v on m.Aerolinea_Codigo = v.Aerolinea_Codigo
+        and m.Aeropuerto_Salida_Codigo = v.Aeropuerto_Salida_Codigo 
+        and m.Aeropuerto_Llegada_Codigo = v.Aeropuerto_Llegada_Codigo 
+        and m.Vuelo_Fecha_Salida = v.Fecha_Salida
+    where m.Propuesta_Nro_Propuesta is not null and m.Detalle_Propuesta_Vuelo_Precio is not null;
+end
+go
+
+create procedure [DB_LOPERS].Migrar_Propuesta_Hospedaje
+as
+begin
+    insert into [DB_LOPERS].Propuesta_Hospedaje (Propuesta_Nro_Propuesta, ID_Habitacion, Fecha_Desde, Fecha_Hasta, Cantidad_Habitaciones, Precio, Subtotal)
+    select distinct 
+        m.Propuesta_Nro_Propuesta, 
+        h.ID_Habitacion, 
+        m.Detalle_Propuesta_Hospedaje_Fecha_Desde, 
+        m.Detalle_Propuesta_Hospedaje_Fecha_Hasta, 
+        m.Detalle_Propuesta_Hospedaje_Cant, 
+        m.Detalle_Propuesta_Hospedaje_Precio, 
+        m.Detalle_Propuesta_Hospedaje_Subtotal
+    from gd_esquema.Maestra m
+    join [DB_LOPERS].Hospedaje ho on m.Hospedaje_Nombre = ho.Nombre and m.Hospedaje_Direccion = ho.Direccion
+    join [DB_LOPERS].Habitacion h on m.Habitacion_Nombre = h.Nombre and ho.ID_Hospedaje = h.ID_Hospedaje
+    where m.Propuesta_Nro_Propuesta is not null and m.Detalle_Propuesta_Hospedaje_Precio is not null;
+end
+go
+
+create procedure [DB_LOPERS].Migrar_Venta
+as
+begin
+    insert into [DB_LOPERS].Venta (Nro_Venta, ID_Cliente, Agente_Legajo, ID_Canal_Venta, ID_Medio_Pago, Fecha_Venta, Subtotal, Descuento, Importe_Total)
+    select distinct 
+        m.Venta_Nro_Venta, 
+        c.ID_Cliente, 
+        m.Agente_Legajo, 
+        cv.ID_Canal_Venta, 
+        mp.ID_Medio_Pago, 
+        m.Venta_Fecha_Venta, 
+        m.Venta_Subtotal, 
+        m.Venta_Descuento, 
+        m.Venta_Importe_Total
+    from gd_esquema.Maestra m
+    join [DB_LOPERS].Cliente c on m.Cliente_Dni = c.Dni
+    join [DB_LOPERS].Canal_Venta cv on m.Venta_Canal_Venta = cv.Descripcion
+    join [DB_LOPERS].Medio_Pago mp on m.Venta_Medio_Pago = mp.Descripcion
+    where m.Venta_Nro_Venta is not null;
+end
+go
+
+create procedure [DB_LOPERS].Migrar_Venta_Propuesta
+as
+begin
+    insert into [DB_LOPERS].Venta_Propuesta (Venta_Nro_Venta, Propuesta_Nro_Propuesta)
+    select distinct 
+        m.Venta_Nro_Venta, 
+        m.Propuesta_Nro_Propuesta
+    from gd_esquema.Maestra m
+    where m.Venta_Nro_Venta is not null and m.Propuesta_Nro_Propuesta is not null;
+end
+go
+
+create procedure [DB_LOPERS].Migrar_Venta_Vuelo
+as
+begin
+    insert into [DB_LOPERS].Venta_Vuelo (Venta_Nro_Venta, ID_Vuelo, Cantidad_Pasajes, Precio_Unitario, Subtotal, Cod_Reserva)
+    select distinct 
+        m.Venta_Nro_Venta, 
+        v.ID_Vuelo, 
+        m.Detalle_Venta_Vuelo_Cantidad_Pasajes, 
+        m.Detalle_Venta_Vuelo_Precio_Unitario, 
+        m.Detalle_Venta_Vuelo_Subtotal, 
+        m.Detalle_Venta_Vuelo_Cod_Reserva
+    from gd_esquema.Maestra m
+    join [DB_LOPERS].Vuelo v on m.Aerolinea_Codigo = v.Aerolinea_Codigo 
+        and m.Aeropuerto_Salida_Codigo = v.Aeropuerto_Salida_Codigo 
+        and m.Aeropuerto_Llegada_Codigo = v.Aeropuerto_Llegada_Codigo 
+        and m.Vuelo_Fecha_Salida = v.Fecha_Salida
+    where m.Venta_Nro_Venta is not null and m.Detalle_Venta_Vuelo_Precio_Unitario is not null;
+end
+go
+
+create procedure [DB_LOPERS].Migrar_Venta_Hospedaje
+as
+begin
+    insert into [DB_LOPERS].Venta_Hospedaje (Venta_Nro_Venta, ID_Habitacion, Fecha_Desde, Fecha_Hasta, Cantidad_Habitaciones, Precio_Unitario, Subtotal, Cod_Reserva)
+    select distinct 
+        m.Venta_Nro_Venta, 
+        h.ID_Habitacion, 
+        m.Detalle_Venta_Hospedaje_Fecha_Desde, 
+        m.Detalle_Venta_Hospedaje_Fecha_Hasta, 
+        m.Detalle_Venta_Hospedaje_Cantidad, 
+        m.Detalle_Venta_Hospedaje_Precio_Unitario, 
+        m.Detalle_Venta_Hospedaje_Subtotal, 
+        m.Detalle_Venta_Hospedaje_Cod_Reserva
+    from gd_esquema.Maestra m
+    join [DB_LOPERS].Hospedaje ho on m.Hospedaje_Nombre = ho.Nombre and m.Hospedaje_Direccion = ho.Direccion
+    join [DB_LOPERS].Habitacion h on m.Habitacion_Nombre = h.Nombre and ho.ID_Hospedaje = h.ID_Hospedaje
+    where m.Venta_Nro_Venta is not null and m.Detalle_Venta_Hospedaje_Precio_Unitario is not null;
+end
+go
+
+create procedure [DB_LOPERS].Migrar_Venta_Excursion
+as
+begin
+    insert into [DB_LOPERS].Venta_Excursion (Venta_Nro_Venta, ID_Excursion, Fecha_Reserva, Cantidad_Excursiones, Precio_Unitario, Subtotal, Cod_Reserva)
+    select distinct 
+        m.Venta_Nro_Venta, 
+        e.ID_Excursion, 
+        m.Detalle_Venta_Excursion_Fecha_Reserva, 
+        m.Detalle_Venta_Excursion_Cant, 
+        m.Detalle_Venta_Excursion_Precio_Unitario, 
+        m.Detalle_Venta_Excursion_Subtotal, 
+        m.Detalle_Venta_Excursion_Cod_Reserva
+    from gd_esquema.Maestra m
+    join [DB_LOPERS].Proveedor p on m.Proveedor_Nombre = p.Nombre
+    join [DB_LOPERS].Excursion e on m.Excursion_Nombre = e.Nombre and m.Excursion_Horario = e.Horario and p.ID_Proveedor = e.ID_Proveedor
+    where m.Venta_Nro_Venta is not null and m.Detalle_Venta_Excursion_Precio_Unitario is not null;
+end
+go
+
+create procedure [DB_LOPERS].Migrar_Encuesta
+as
+begin
+    insert into [DB_LOPERS].Encuesta (Codigo_Encuesta, ID_Cliente, Agente_Legajo, Fecha_Encuesta, Comentarios)
+    select distinct 
+        m.Encuesta_Codigo_Encuesta, 
+        c.ID_Cliente, 
+        m.Agente_Legajo, 
+        m.Encuesta_Fecha_Encuesta, 
+        m.Encuesta_Comentarios
+    from gd_esquema.Maestra m
+    join [DB_LOPERS].Cliente c on m.Cliente_Dni = c.Dni
+    where m.Encuesta_Codigo_Encuesta is not null;
+end
+go
+
+create procedure [DB_LOPERS].Migrar_Detalle_Encuesta
+as
+begin
+    insert into [DB_LOPERS].Detalle_Encuesta (Encuesta_Codigo_Encuesta, ID_Aspecto, Puntaje)
+    select distinct 
+        m.Encuesta_Codigo_Encuesta, 
+        a.ID_Aspecto, 
+        m.Detalle_Encuesta_Puntaje
+    from gd_esquema.Maestra m
+    join [DB_LOPERS].Aspecto a on m.Aspecto_Aspecto = a.Descripcion
+    where m.Encuesta_Codigo_Encuesta is not null and m.Aspecto_Aspecto is not null;
+end
+go
